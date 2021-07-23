7f0720ea9897c118a13143c6c2da9d42a006653e
Lmanti <luisangelherreramantilla@gmail.com>
2021-07-23 00:16:26 -0500

    settings to deploy adding a port

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const server = require('./src/app.js');
-const { Port } = require('./src/utils/config/index.js');
 
-server.listen(Port || 3001, () => {
-    console.log("Server listening at 3001")
+server.listen(process.env.PORT || 3001, () => {
+    console.log(`SERVER LISTENING AT ${process.env.PORT}`)
 })
